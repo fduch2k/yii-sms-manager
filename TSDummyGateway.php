b31d11c4360509a7e0584403cf8e1680e9e62e20
Alexander Hramov <alexander.hramov@gmail.com>
2014-09-11 15:18:25 +0400

    Removed unused method getParams

--- TSDummyGateway.php
+++ TSDummyGateway.php
@@ -8,11 +8,6 @@ class TSDummyGateway extends TSSmsGateway
         return "DummySms";
     }
 
-    private function getParams()
-    {
-        return Yii::app()->params['sms']['TSDummyGateway'][Yii::app()->language];
-    }
-
     public function send($phones, $text)
     {
         return array();
