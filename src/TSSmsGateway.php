8dc5150deec99b58d75efea944233718f339af93
Alexander Hramov <alexander.hramov@gmail.com>
2014-09-16 12:10:01 +0400

    Added type casting to float for minCredit param

--- src/TSSmsGateway.php
+++ src/TSSmsGateway.php
@@ -1,5 +1,15 @@
 <?php
 
+/**
+ * SMS Gateway base class
+ *
+ * @link https://github.com/fduch2k/yii-sms-manager
+ * @author Alexander Hramov <alexander.hramov@gmail.com>
+ * @author Victor Tyurin
+ * @copyright Copyright (c) 2012-2014, TagShake Ltd.
+ * @license http://opensource.org/licenses/MIT
+ */
+
 abstract class TSSmsGateway extends CComponent
 {
     public $username;
@@ -10,4 +20,9 @@ abstract class TSSmsGateway extends CComponent
     abstract public function send($phone, $text);
     abstract public function command($commands, $params);
     abstract public function getName();
+
+    public function init()
+    {
+        $this->minCredit = CPropertyValue::ensureFloat($this->minCredit);
+    }
 }
